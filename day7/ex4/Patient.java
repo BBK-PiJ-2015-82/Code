ef9a574a84151fc8ae2e2300583a43009eb5affb
Danny Madell <dmadell@premierit.com>
2015-11-13 17:12:35 +0000

    end of work 13th nov

---
+++ day7/ex4/Patient.java
@@ -0,0 +1,53 @@
+public class Patient {
+	private String name;
+	private int age;
+	private String illness;
+	private Patient nextPatient;
+	private Patient prevPatient;
+
+	public Patient(String name, int age, String illness){
+
+		this.name = name;
+		this.age = age;
+		this.illness = illness;
+	}
+
+	public Patient getNext() {
+		return nextPatient;
+	}
+
+	public void setNext(Patient next) {
+		this.nextPatient = next;
+
+	}
+
+	public void setPrev(Patient prev) {
+		this.prevPatient = prev;
+
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void printPatients() {
+		if (this == null) {
+			System.out.println("There is nothing to print");
+		} 
+		else
+		{
+			if (this.nextPatient == null) {
+				System.out.println(this.toString());
+			} else {
+				System.out.println(this.toString());
+				this.nextPatient.printPatients();	
+			}	
+		}
+	} 	
+	
+	public String toString() {
+		String returnValue = "";
+		returnValue = "" + this.name + ", " + this.age + ", " + this.illness;
+		return returnValue;
+	} 
+}
