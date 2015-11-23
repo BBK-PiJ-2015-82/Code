d10a347f09154b8ddb6b179ad8d7faeff34d0bf1
unknown <dmadel01@CSPC041.dcs.bbk.ac.uk>
2015-11-23 20:52:00 +0000

    End of Monday week 9

--- day9/ex1/IntegerTreeNode.java
+++ day9/ex1/IntegerTreeNode.java
@@ -84,8 +84,12 @@ public class IntegerTreeNode {
 				break;
 			}
 			current = current.left;
-		}	while (left != null);
+		}	while (left != null);		
 		return lowest.value;
+
+		/*
+		return left.getMin()
+		*/
 	}
 /*
 	public String toString() {
@@ -105,14 +109,91 @@ public class IntegerTreeNode {
 
 	public String toString() {
 		//start string
+		String returnString = "[" + this.value;
+		if(left != null) {returnString += " L" + left.toString() + "]" + " R[ ";}
+		else {returnString += "R ";}
+		if(right != null) {returnString += right.toString();}
+		else {returnString += "]";}
+		returnString += " ]";
+		return returnString;
+	}
+
+	public int depth() {
+		int leftVal = 0;
+		int rightVal = 0;
+		if (left == null && right == null) {
+			return 1;
+		}
+		if (left != null) {
+			//rightVal++;
+			leftVal = 1 + le0ft.depth();
+		}
+		if (right != null) {
+			rightVal = 1 + right.depth();
+			//leftVal++;
+		}
+		if (rightVal > leftVal) {
+			return rightVal;
+		} else {
+			return leftVal;
+		}
+	}
+
+	public int depthSergio() {
+		int leftDepth = 0;
+		if (left != null) {
+			leftDepth = left.depthSergio();
+		}
+		int rightDepth = 0;
+		if (right != null) {
+			rightDepth = right.depthSergio();
+		}
+		if (leftDepth > rightDepth) {
+			return 1 + leftDepth;
+		} else {
+			return 1 + rightDepth;
+		}
+	}
+/*
+	public String toStringLogic() {
 		String returnString = "";
 		//returnString = returnString.concat(toString());
 		//return value for the end of the recursion when no left or right
-		
-		returnString = returnString.concat("[" + this.value + "]");
-			returnString = returnString.concat("L[" + left.toString());
-			returnString = returnString.concat("R[" + right.toString());
+		returnString = returnString.concat("" + this.value + " L[");
+		/*if (left == null && right == null) {
+			returnString = returnString.concat("[" + this.value + "L" "]");
+		} else { 
+		if (left == null) {
+			returnString = returnString.concat("] R[");
+		} else {
+			returnString += left.toString();
+		}
+		if (right == null) {
+			returnString += "]";
+		} else {
+			returnString += right.toString();
+		}
 
+
+		return returnString;
+	}
+	
+*/
+			/*
+			returnString = returnString.concat("[" + this.value);
+			if (this.left != null) {
+				returnString = returnString.concat(left.toString() + "] ");
+			} else {
+				returnString = returnString.concat("]");
+			}
+			if (this.right != null) {
+				returnString = returnString.concat(right.toString() + "] ");
+			} else {
+				returnString = returnString.concat("]");
+			}
+			//returnString = returnString.concat("]");
+
+		}
 		/*if (this.left != null) {
 			returnString = returnString.concat("L[" + left.toString());
 		} else if (this.right != null) {
@@ -122,10 +203,9 @@ public class IntegerTreeNode {
 		} **/
 
 
-		return returnString;
 
 		
-	}
+	
 		/*
 		//if left add that
 		if (this.left == null && this.right == null) {
@@ -159,24 +239,28 @@ ABANDONED RECURSION METHOD, CAN'T WORK OUT HOW TO COMPARE VALUES BEING RETURN
 	**/
 	public void launch() {
 		
-		add(99);
-		add(99);
-		add(1111);
-		add(4);
-		add(12);
-		add(6);
-		add(7);
-		add(55);
-		add(9);
-		add(10);
-		add(11);
 		
-		add(13);
-		add(3);
+		add(2);
+		add(4);
+		add(6);
+		
+		add(7);
+				add(9);
+								add(10);
+								add(8);
+
+								add(877);
+
+
+
 		//System.out.println(this.left.right.value);
 		System.out.println(this.getMax());
 		System.out.println(this.getMin());
-		System.out.println(toString());
+		System.out.println(this.toString());
+				System.out.println(this.depth());
+								System.out.println(depthSergio());
+
+
 
 
 		//System.out.println(this.right.right.right.value);
