f58c92dd665f764ac2ccd92cf7f0858d64263e3c
unknown <dmadel01@CSPC058.dcs.bbk.ac.uk>
2015-11-30 21:00:25 +0000

    end of day 10 monday lesson

---
+++ day10/ex1,2,3/PhoneLauncher.java
@@ -0,0 +1,44 @@
+public class PhoneLauncher extends SmartPhone {
+
+	public static void main(String[] args) {
+		PhoneLauncher launcher = new PhoneLauncher();
+		launcher.launch();
+	}
+
+	public void launch() {
+
+		call("2342345345");
+		ringAlarm("8.am");
+		playGame("snake");
+		last10Numbers[0] = "123";
+				last10Numbers[1] = "3737";
+
+		last10Numbers[2] = "1234523453";
+
+		last10Numbers[3] = "127673";
+
+		last10Numbers[4] = "121123423";
+
+		last10Numbers[5] = "001456423";
+
+		last10Numbers[6] = "456546488";
+				last10Numbers[7] = "2462";
+
+		last10Numbers[8] = "15838323";
+
+		last10Numbers[9] = "1213453";
+
+		printLastNumbers();
+
+		browseWeb("google");
+		System.out.println(findPosition());
+		call("001456423");
+
+		call("4564671456423");
+		System.out.println(getBrand());
+
+
+
+
+	}
+}
---
+++ day10/ex1,2,3/SmartPhone.java
@@ -0,0 +1,30 @@
+public class SmartPhone extends MobilePhone {
+	
+
+	public SmartPhone() {
+		super("iPhone");
+	}
+
+	public void browseWeb(String str) {
+		System.out.println(str + " on the internet");
+	}
+
+	public String findPosition() {
+		String str = "this is your location";
+		return str;
+	}
+
+
+
+	@Override
+	public void call(String str) {
+		if (str.charAt(0) == '0' && str.charAt(1) == '0') {
+			System.out.println("Calling " +  str + " through the internet to save money");
+		} else {
+			super.call(str);
+		}
+	}
+
+	
+
+}
